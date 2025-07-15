f5e331bcecebd770c0c7ed02541f737360281c01
Radoslaw Zienkiewicz <radoslaw.zien@outlook.com>
2025-07-15 14:37:09 +0100

    Added basic structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,27 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div> 
-      <h1>This is my main portfolio page</h1><br></br>
-      <Link href={'/projects'}>To Projects</Link>
+    <div id = 'root'>
+      <section id = 'overview'>
+        <div>
+          <h1>Overview</h1>
+        </div>
+      </section>
+      <section id = 'projects'>
+        <div>
+          <h1>My Projects</h1>
+        </div>
+      </section>
+      <section id = 'education'>
+        <div>
+          <h1>Education and Qualifications</h1>
+        </div>
+      </section>
+      <section id = 'contact'>
+        <div>
+          <h1>Contact me</h1>
+        </div>
+      </section>
     </div>
   );
 }
