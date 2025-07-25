2a6685a4d76ab6977af197056d3786696923775e
Radoslaw Zienkiewicz <radoslaw.zien@outlook.com>
2025-07-25 19:05:39 +0100

    formatting

--- src/components/Rights.tsx
+++ src/components/Rights.tsx
@@ -3,7 +3,7 @@ export const Rights = () => {
         <>
         <hr className="border-t border-gray-300 my-10 opacity-50 w-3/4 mx-auto" />
         <footer className="flex justify-center text-center text-sm opacity-50">
-            © 2025 Radoslaw Zienkiewicz. All rights reserved.<br />
+        © 2025 Radoslaw Zienkiewicz. All rights reserved.<br />
 
         Logos and trademarks used on this site are the property of their respective owners. <br />
         Used with permission or under fair use for informational purposes only.
