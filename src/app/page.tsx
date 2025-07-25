76c1ab8221bfa9b6afa75588714e62656d9cf1df
Radoslaw Zienkiewicz <radoslaw.zien@outlook.com>
2025-07-25 17:16:06 +0100

    Padding to root component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { Contact } from "@/components/Contact";
 
 export default function Home() {
   return ( 
-    <div id = 'root'>
+    <div id = 'root' className="p-2">
 
     <div id= 'nav'>
       <NavBar/>
