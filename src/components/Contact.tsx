80cd5e447d9ca0080083fe828764da93f68e7e76
Radoslaw Zienkiewicz <radoslaw.zien@outlook.com>
2025-07-29 19:02:51 +0100

    Updated contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -9,12 +9,12 @@ export const Contact = () => {
           method="POST"
           className="flex flex-col space-y-4 bg-[#383535] rounded-md p-6 shadow-md"
         >
-            <p className="font-bold text-lg">Fill out this form to send me an email! Please include your email if you wish for me to reply</p>
+            <p className="font-bold text-lg">Fill out this form to get in touch with me!</p>
           <label className="text-sm font-medium text-white">
-            Name
+            E-mail
             <input
               type="text"
-              name="name"
+              name="email"
               required
               className="w-full border border-gray-300 rounded px-3 py-2 mt-1"
             />
