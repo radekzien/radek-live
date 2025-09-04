37e9fd4c8d46a9495d3e8074b469d72dd09da709
radekzien <radoslaw.zien@outlook.com>
2025-09-04 13:13:53 +0100

    additional security

--- scripts/generate-token.ts
+++ scripts/generate-token.ts
@@ -12,6 +12,7 @@ const oauth2Client = new google.auth.OAuth2(
 const authUrl = oauth2Client.generateAuthUrl({
   access_type: "offline",
   scope: ["https://www.googleapis.com/auth/gmail.readonly"],
+  prompt: "consent"
 });
 
 console.log(authUrl);
